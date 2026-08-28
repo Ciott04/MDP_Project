928ff96e1b16b1cc3546f8add79b94d41420e409
Vincenzo <vince.marinelli04@gmail.com>
2026-08-28 14:14:54 +0200

    Implementazione classe Monster

---
+++ src/main/java/it/unicam/cs/mpgc/rpg125671/model/Monster.java
@@ -0,0 +1,17 @@
+package it.unicam.cs.mpgc.rpg125671.model;
+
+public class Monster extends GameCharacter {
+
+    private final int expReward;
+
+    public Monster(String name, int maxHp, int attack, int defense, int speed, int expReward) {
+        super(name, maxHp, attack, defense, speed);
+        if (expReward <= 0)
+            throw new IllegalArgumentException("La ricompensa in esperienza deve essere maggiore di 0.");
+        this.expReward = expReward;
+    }
+
+    public int getExpReward() {
+        return expReward;
+    }
+}
