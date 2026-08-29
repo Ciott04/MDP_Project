c9c662f142bb50120d3a8e07ab304ada544e814f
Vincenzo <vince.marinelli04@gmail.com>
2026-08-29 17:34:10 +0200

    Fix nome pozione

--- src/main/java/it/unicam/cs/mpgc/rpg125671/model/HealingPotion.java
+++ src/main/java/it/unicam/cs/mpgc/rpg125671/model/HealingPotion.java
@@ -3,7 +3,7 @@ package it.unicam.cs.mpgc.rpg125671.model;
 public class HealingPotion implements Item {
 
     private static final int HEAL_AMOUNT = 30;
-    private static final String NAME = "Pozione Curativa";
+    private static final String NAME = "Pozione curativa";
     @Override
     public String getName() {
         return NAME;
