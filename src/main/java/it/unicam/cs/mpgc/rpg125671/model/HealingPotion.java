8c53df8582df2877b2b719c4b1d6c3f492916f47
Vincenzo <vince.marinelli04@gmail.com>
2026-09-09 01:00:04 +0200

    Fix HealingPotion

--- src/main/java/it/unicam/cs/mpgc/rpg125671/model/HealingPotion.java
+++ src/main/java/it/unicam/cs/mpgc/rpg125671/model/HealingPotion.java
@@ -2,7 +2,7 @@ package it.unicam.cs.mpgc.rpg125671.model;
 
 public class HealingPotion implements Item {
 
-    private static final int HEAL_AMOUNT = 50;
+    private static final int HEAL_AMOUNT = 30;
     private static final String NAME = "Pozione curativa";
     @Override
     public String getName() {
