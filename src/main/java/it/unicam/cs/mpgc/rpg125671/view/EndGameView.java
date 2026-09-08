9127bc3ec98658b8a8333a7ee3de5b0fd5353e56
Vincenzo <vince.marinelli04@gmail.com>
2026-09-08 22:59:17 +0200

    Implementazione classe GameApp

---
+++ src/main/java/it/unicam/cs/mpgc/rpg125671/view/EndGameView.java
@@ -0,0 +1,4 @@
+package it.unicam.cs.mpgc.rpg125671.view;
+
+public class EndGameView {
+}
---
+++ src/main/java/it/unicam/cs/mpgc/rpg125671/view/GameApp.java
@@ -0,0 +1,70 @@
+package it.unicam.cs.mpgc.rpg125671.view;
+
+import it.unicam.cs.mpgc.rpg125671.engine.GameEngine;
+import it.unicam.cs.mpgc.rpg125671.persistence.JsonSaveManager;
+import it.unicam.cs.mpgc.rpg125671.persistence.SaveManager;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GameApp extends Application {
+
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+    private static final String SAVE_NAME = "savegame";
+
+    private Stage stage;
+    private GameEngine gameEngine;
+    private final SaveManager saveManager = new JsonSaveManager();
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.stage = stage;
+        stage.setTitle("Dungeon of the Forgotten");
+        stage.setScene(new Scene(new MainMenuView(this), WIDTH, HEIGHT));
+    }
+
+    void switchView(Parent view) {
+        stage.getScene().setRoot(view);
+    }
+
+    // --- Navigazione ---
+
+    void showMainMenu() {
+        switchView(new MainMenuView(this));
+    }
+    void showHeroSelection() {
+        switchView(new HeroSelectionView(this));
+    }
+
+    void showExploration() {
+        switchView(new ExplorationView(this));
+    }
+
+    void showCombat() {
+        switchView(new CombatView(this));
+    }
+
+    void showEndGame(boolean won) {
+        switchView(new EndGameView(this, won));
+    }
+
+    // ---Accesso allo stato ---
+    GameEngine getGameEngine() {
+        return gameEngine;
+    }
+
+    void setGameEngine(GameEngine gameEngine) {
+        this.gameEngine = gameEngine;
+    }
+
+    SaveManager getSaveManager() {
+        return saveManager;
+    }
+
+    String getSaveName() {
+        return SAVE_NAME;
+    }
+
+}
